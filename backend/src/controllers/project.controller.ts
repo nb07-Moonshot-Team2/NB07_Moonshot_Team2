2a7afd8cc3d9fe67691ee902965d0e5552aae534
ktmgw486 <ktmgw486@gmail.com>
2026-02-04 06:28:23 +0000

    #3 #25 #26 #28 #29 fix:프로젝트 등록,상세보기,수정,삭제 시능 수정

--- backend/src/controllers/project.controller.ts
+++ backend/src/controllers/project.controller.ts
@@ -11,18 +11,18 @@ export async function createProject(req :Request,res:Response){
 };
 export async function getProject(req :Request,res:Response){
     const projectId = req.params.projectId;
-    const project = await projectService.getProject(projectId);
+    const project = await projectService.getProject(projectId as string);
     res.status(200).send(project);
 };
 export async function updateProject(req :Request,res:Response){
     const projectId = req.params.projectId;
     const body = req.body as CreatedProjectInput;
-    const updatedProject = await projectService.updateProject(projectId,body);
+    const updatedProject = await projectService.updateProject(projectId as string,body);
     res.status(200).send(updatedProject);                                                                                  
 }
 
 export async function deleteProject(req :Request,res:Response){
     const projectId = req.params.projectId;
-    const deletedProject = await projectService.deleteProject(projectId);
+    const deletedProject = await projectService.deleteProject(projectId as string);
     res.status(204).send(deletedProject);
 }       
